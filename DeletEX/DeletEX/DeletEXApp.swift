118c2dd3ff1d6f5cde0d7bbbdf9138e3936a691e
Marko Jovanov <marko.jovanov@netcetera.com>
2024-07-06 01:30:46 +0200

    Inital project setup

---
+++ DeletEX/DeletEX/DeletEXApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeletEXApp.swift
+//  DeletEX
+//
+//  Created by Marko Jovanov on 6.7.24.
+//
+
+import SwiftUI
+
+@main
+struct DeletEXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
